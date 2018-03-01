074cd4948cb2b40599ba1de14d7e7d39da5b679b
Luiz Felipe <lflimeira02@gmail.com>
2018-02-28 22:21:53 -0300

    Implementação do FizzBuzz

--- dojos/tc-talks-tdd-js/index.js
+++ dojos/tc-talks-tdd-js/index.js
@@ -1,1 +1,14 @@
-console.log('Teste')
+function fizzbuzz(array) {
+   return array.map((number) => {
+        var message = ''
+        if (number % 3 == 0) {
+            message += `Fizz`;        
+        }
+        if (number % 5 == 0) {
+            message += `Buzz`;        
+        }
+        return message==""?number:message;
+    })
+}
+
+module.exports = fizzbuzz
--- dojos/tc-talks-tdd-js/tests/sample.test.js
+++ dojos/tc-talks-tdd-js/tests/sample.test.js
@@ -1,9 +1,30 @@
 var assert = require('assert');
+var fizzbuzz = require('../index.js');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+describe('Implementação do FizzBuzz', function() {
+  describe('Fizz', function() {
+    const ary = [];
+    before(function() {
+      for (let i = 1; i <= 100; i++) {
+        ary.push(i);
+      }
+    })   
+    
+    it('should return Fizz when number is divisible by 3', function() {
+      var result = fizzbuzz(ary);
+      assert.equal(result[2], 'Fizz');
+    });
+    it('should return Buzz when number is divisible by 5', function() {
+      var result = fizzbuzz(ary);
+      assert.equal(result[4], 'Buzz');
+    });
+    it('should return FizzBuzz when number is divisible by 3 and 5', function() {
+      var result = fizzbuzz(ary);
+      assert.equal(result[14], 'FizzBuzz');
+    });
+    it('should return the number', function() {
+      var result = fizzbuzz(ary);
+      assert.equal(result[3], 4);
     });
   });
 });
